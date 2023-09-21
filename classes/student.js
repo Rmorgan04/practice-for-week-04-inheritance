f38f2326df888c110982c56a3b7af4f920517ad0
Robby Morgan <robby.d.morgan@gmail.com>
2023-09-21 10:37:46 -0500

    Final Submit

--- classes/student.js
+++ classes/student.js
@@ -1,6 +1,29 @@
 const Person = require('./person');
 
-// Your code here
+class Student extends Person {
+  constructor(firstName, lastName, major, GPA) {
+    super(firstName, lastName);
+    this.major = major;
+    this.GPA = GPA;
+  }
+
+  static compareGPA(st1, st2) {
+    if (st1.GPA < st2.GPA) {
+      return st2.firstName + ' ' + st2.lastName + ' has the higher GPA.'
+      
+    }
+    else if (st1.GPA > st2.GPA) {
+      return st1.firstName + " " + st1.lastName + " has the higher GPA."
+    }
+    else {
+      return "Both students have the same GPA";
+    }
+  }
+
+}
+
+
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- classes/teacher.js
+++ classes/teacher.js
@@ -1,6 +1,30 @@
 const Person = require('./person');
 
-// Your code here
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName,);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teachers) {
+    let sum = 0;
+    for (const teacher of teachers) {
+      sum += Number(teacher.yearsOfExperience);
+    }
+    return sum;
+  }
+
+}
+
+//Examples:
+
+// const reb = new Teacher ('Reb', "Tevya", '41', 'English', '12' )
+// const emma = new Teacher ('Emma', "Foyer", '34', 'Parapsychology', '6' )
+// const dan = new Teacher ('Dan', "Foyer", '35', 'Film', '8' )
+// let teachers = [reb, emma, dan]
+
+// console.log(Teacher.combinedYearsOfExperience(teachers));
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
